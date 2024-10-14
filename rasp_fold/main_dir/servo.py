3e00aa4c0c70e92cd764f5106d70e7b9752ca383
zeculesu <artiyjana@yandex.ru>
2024-10-14 12:37:24 +0300

    refactoring code, adding camera/manipulator controller

--- rasp_fold/main_dir/servo.py
+++ rasp_fold/main_dir/servo.py
@@ -11,68 +11,19 @@ def angle_limit(angle):
 
 
 class ServoController(object):
-    cur_main_angle = cfg.MAIN_UP
-    cur_cubit_angle = cfg.CUBIT_DEFAULT
-    cur_wrist_angle = cfg.WRIST_ROTATED
-    cur_claw_angle = cfg.OPENED_CLAW
 
     def __init__(self):
-        super().__init__()
         self.i2c = I2c()
-        self.set_default_position()
-        self.close_claw()
 
     def set_default_position(self):
-        self.cur_main_angle = cfg.MAIN_UP
-        self.cur_cubit_angle = cfg.CUBIT_DEFAULT
-        self.cur_wrist_angle = cfg.WRIST_ROTATED
-        self.set_position()
-
-    def close_claw(self):
-        self.cur_claw_angle = cfg.CLOSED_CLAW
-        self.set_position()
-
-    def open_claw(self):
-        self.cur_claw_angle = cfg.OPENED_CLAW
-        self.set_position()
-
-    def set_throw_position(self):
-        self.cur_main_angle = cfg.MAIN_UP
-        self.cur_cubit_angle = cfg.CUBIT_THROW
-        self.cur_wrist_angle = cfg.WRIST_ROTATED
-        self.set_position()
-
-    def set_down_position(self):
-        self.cur_main_angle = cfg.MAIN_DOWN
-        self.cur_cubit_angle = cfg.CUBIT_DEFAULT
-        self.cur_wrist_angle = cfg.WRIST_STRAIGHT
-        self.set_position()
-
-    def move_main(self, angle):
-        self.cur_main_angle = angle_limit(angle + self.cur_main_angle)
-        self.set_position()
-
-    def move_cubit(self, angle):
-        self.cur_cubit_angle = angle_limit(angle + self.cur_cubit_angle)
-        self.set_position()
-
-    def move_wrist(self, angle):
-        self.cur_wrist_angle = angle_limit(angle + self.cur_wrist_angle)
-        self.set_position()
-
-    def move_claw(self, angle):
-        self.cur_main_angle = angle_limit(angle + self.cur_claw_angle)
-        self.set_position()
+        pass
 
     def set_position(self):
-        self.set(cfg.MAIN_PORT, self.cur_main_angle)
-        self.set(cfg.CUBIT_PORT, self.cur_cubit_angle)
-        self.set(cfg.WRIST_PORT, self.cur_wrist_angle)
-        self.set(cfg.CLAW_PORT, self.cur_claw_angle)
+        pass
 
     def set(self, servo_port, angle):
         angle = angle_limit(angle)
-        buf = [0xff, 0x01, servo_port, angle, 0xff]
+        buf = [cfg.HEADER, 0x01, servo_port, angle, cfg.HEADER]
         try:
             self.i2c.write_data(self.i2c.mcu_address, buf)
         except Exception as e:
--- rasp_fold/main_dir/test.py
+++ rasp_fold/main_dir/test.py
@@ -1,60 +1,73 @@
 from time import sleep
 
 import motor
-import servo
+import manipulator
+import camera
 
 mc = motor.MotorController()
-sc = servo.ServoController()
+manipulator_cont = manipulator.ManipulatorController()
+camera_cont = camera.CameraController()
 
-command = int(input())
-while command != 0:
-    sc.set(8, command)
-    command = int(input())
-
-command = int(input())
-while command != 0:
-    sc.set(7, command)
-    command = int(input())
-
-command = int(input())
-while command != 0:
-    sc.set(6, command)
-    command = int(input())
-
-command = int(input())
-while command != 0:
-    sc.set(5, command)
-    command = int(input())
-
-command = int(input())
-while command != 0:
-    sc.set(4, command)
-    command = int(input())
-
-command = int(input())
-while command != 0:
-    sc.set(3, command)
-    command = int(input())
-
-command = int(input())
-while command != 0:
-    sc.set(2, command)
-    command = int(input())
-
-command = int(input())
-while command != 0:
-    sc.set(1, command)
-    command = int(input())
+# command = int(input())
+# while command != 0:
+#     sc.set(8, command)
+#     command = int(input())
+#
+# command = int(input())
+# while command != 0:
+#     sc.set(7, command)
+#     command = int(input())
+#
+# command = int(input())
+# while command != 0:
+#     sc.set(6, command)
+#     command = int(input())
+#
+# command = int(input())
+# while command != 0:
+#     sc.set(5, command)
+#     command = int(input())
+#
+# command = int(input())
+# while command != 0:
+#     sc.set(4, command)
+#     command = int(input())
+#
+# command = int(input())
+# while command != 0:
+#     sc.set(3, command)
+#     command = int(input())
+#
+# command = int(input())
+# while command != 0:
+#     sc.set(2, command)
+#     command = int(input())
+#
+# command = int(input())
+# while command != 0:
+#     sc.set(1, command)
+#     command = int(input())
 
 mc.forward_on_steps(3)
 mc.right_on_place_on_steps(3)
 mc.left_on_place_on_steps(3)
 mc.backward_on_steps(3)
 
-sc.open_claw()
-sc.set_default_position()
-sleep(2)
-sc.set_down_position()
-sc.open_claw()
-sleep(2)
-sc.set_throw_position()
+# sc.open_claw()
+# sc.set_default_position()
+# sleep(2)
+# sc.set_down_position()
+# sc.open_claw()
+# sleep(2)
+# sc.set_throw_position()
+
+while True:
+    camera_cont.move_cubit(50)
+    sleep(1)
+    camera_cont.move_rotate(50)
+    sleep(1)
+    camera_cont.move_rotate(-50)
+    sleep(1)
+    camera_cont.move_cubit(-50)
+    sleep(1)
+
