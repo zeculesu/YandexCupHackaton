23ab0ea8551eb0833677da0fd1088a70fd9f96d4
zeculesu <artiyjana@yandex.ru>
2024-10-15 13:42:25 +0300

    architecture refactor

--- infr_sensor.py
+++
@@ -1,7 +0,0 @@
-import xr_gpio as gpio
-
-for i in range(3):
-    dist = input()
-    print(gpio.digital_read(gpio.IR_L))
-    print(gpio.digital_read(gpio.IR_M))
-    print(gpio.digital_read(gpio.IR_R))
---
+++ workspace_rsp/start.py
@@ -0,0 +1,7 @@
+import buzzer
+import infrared
+import ultrasonic
+from rgb_panel import RGBPanel
+from motor import
+
+import config as cfg
