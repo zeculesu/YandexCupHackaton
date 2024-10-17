6af204dd34ad8e441235222cab371a3dd481462c
Ilushk-cher <ils@ch-5.ru>
2024-10-17 18:02:34 +0500

    do videocapture from robot cam

--- red_cube_detection.py
+++ red_cube_detection.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-from client.sender import Sender
+import requests
 
-
+url = "http://192.168.2.156:8080/?action=stream"
 def find_red_cube(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -33,35 +33,42 @@ def find_red_cube(frame):
 
     return center
 
+stream = requests.get(url, stream=True)
+if stream.status_code == 200:
+    bytes = bytes()
+    for chunk in stream.iter_content(chunk_size=1024):
+        bytes += chunk
+        a = bytes.find(b'\xff\xd8') # Начало jpeg
+        b = bytes.find(b'\xff\xd9') # Конец jpeg
+        if a != -1 and b != -1:
+            jpg = bytes[a:b + 2]
+            bytes = bytes[b + 2:]
+            img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2. IMREAD_COLOR)
 
-host, port = "192.168.2.156", 4242
-sender = Sender(host, port)
-while True:
-    try:
-        is_connect = sender.start_client()
-        if is_connect:
-            break
-    except:
-        continue
+            center = find_red_cube(img)
+            if center is not None:
+                cv2.circle(img, center, 5, (0, 255, 0), -1)
+                cv2.putText(img, "Center: ({}, {})".format(center[0], center[1]), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                                (0, 0, 255), 2)
 
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    raise IOError("Не удалось открыть видеопоток")
+            cv2.imshow('img', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-while (True):
-    ret, frame = cap.read()
-
-    center = find_red_cube(frame)
-
-    if center is not None:
-        cv2.circle(frame, center, 5, (0, 255, 0), -1)
-        cv2.putText(frame, "Center: ({}, {})".format(center[0], center[1]), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    (0, 0, 255), 2)
-
-    cv2.imshow('Video', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+# while (True):
+#     ret, frame = cap.read()
+#
+#     center = find_red_cube(frame)
+#
+#     if center is not None:
+#         cv2.circle(frame, center, 5, (0, 255, 0), -1)
+#         cv2.putText(frame, "Center: ({}, {})".format(center[0], center[1]), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+#                     (0, 0, 255), 2)
+#
+#     cv2.imshow('Video', frame)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
