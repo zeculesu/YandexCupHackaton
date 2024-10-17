98c0fd7796a6eaa31b60e4e8b41c12b2dca929fc
zeculesu <artiyjana@yandex.ru>
2024-10-17 20:10:18 +0500

    lolololl

--- send_command_console_python.py
+++ send_command_console_python.py
@@ -2,3 +2,5 @@ from AppOpenCv.App.config import *
 from old.sender import Sender
 
 sender = Sender("192.168.2.156", 4141)
+sender.start_client()
+# exec(open("send_command_console_python.py").read())
