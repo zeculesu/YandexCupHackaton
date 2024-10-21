951cb16e8d1e850f6a5b2be7009bd092f52c6534
Ilushk-cher <ils@ch-5.ru>
2024-10-21 18:05:03 +0500

    AAAA

--- AppOpenCv/App/AI.py
+++ AppOpenCv/App/AI.py
@@ -2,6 +2,9 @@ import cv2 as cv
 from ultralytics import YOLO
 import move_n_grab_algo
 from sender import Sender
+import torch
+torch.backends.cudnn.enabled = False
+torch.backends.cudnn.benchmark = False
 
 class AI:
     def __init__(self):
@@ -12,6 +15,7 @@ class AI:
 
     def live_ai(self, frame, scenario):
         results = self.model(frame)
+        commands = []
 
         for result in results:
             boxes = result.boxes
@@ -22,7 +26,8 @@ class AI:
                 class_names = self.model.names  # {id: "class_name"}
                 class_name = class_names.get(class_id, "Unknown")
 
-                commands = []
+
+
                 if scenario == 0 and class_id in [2, 3]:
                     x1, y1, x2, y2 = box.xyxy[0]
                     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
@@ -30,10 +35,18 @@ class AI:
                     center_x = (x1 + x2) // 2
                     center_y = (y1 + y2) // 2
 
-                    commands = move_n_grab_algo.push_button(center_x, center_y)
+                    cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv.putText(frame, class_name, (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                    cv.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
 
-                for command in commands:
-                    self.sender.send_command(command)
+
+
+                    commands = move_n_grab_algo.push_button(center_x, center_y)
+        cv.imshow('Live AI', frame)
+        cv.waitKey(1)
+
+        for command in commands:
+            self.sender.send_command(command)
 
                 # Вывод координат всех вершин прямоугольника
                 # print(f'Box (x1, y1, x2, y2): ({x1}, {y1}, {x2}, {y2}), '
@@ -41,9 +54,5 @@ class AI:
                 #       f'Bottom-Left: ({x1}, {y2}), Bottom-Right: ({x2}, {y2})), '
                 #       f'Center: ({center_x}, {center_y}), Class ID: [{class_id}], Class Name: [{class_name}]')
 
-                    cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                    cv.putText(frame, class_name, (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-                    cv.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
 
-        cv.imshow('Live AI', frame)
-        cv.waitKey(1)
+
---
+++ AppOpenCv/App/send_command_console_python.py
@@ -0,0 +1,84 @@
+from AppOpenCv.App.config import *
+
+from AppOpenCv.App.sender import Sender
+import socket
+from time import sleep
+
+
+# пишем sender = Sender(self.host, self.port) потом
+# sender.start_client(), проверяем если там не False, то всё хорошо, мы подключились
+# далее чтобы отправить запрос пишем sender.send( MOTOR.STOP (например) ) если произошла ошибка при отправке сообщения,
+# то вернет False
+# в САМОМ конце работы надо закрыть сокет с помощью sender.socket_close()
+
+
+class Sender324:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = None
+
+    def create_socket(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self):
+        if self.socket is None:
+            self.create_socket()
+        self.socket.connect((self.host, self.port))
+
+    def socket_close(self):
+        if self.socket is not None:
+            self.socket.close()
+        self.socket = None
+
+    def send(self, message):
+        self.socket.send(message.encode('utf-8'))
+        response = self.socket.recv(1024).decode('utf-8')
+        if not response:
+            self.socket_close()
+            return False
+        return response
+
+    def start_client(self):
+        try:
+            self.create_socket()
+            self.connect()
+        except ConnectionError:
+            self.socket_close()
+            return False
+
+        return True
+
+    def try_connection(self):
+        for i in range(5):
+            if self.start_client():
+                return True
+            sleep(5)
+        return False
+
+    def check_connection(self):
+        return True if self.socket else False
+
+    def send_command(self, command):
+        if not self.check_connection():
+            if not self.try_connection():
+                return False
+        resp = self.send(command)
+        return self.handle_response(resp, command)
+
+    def handle_response(self, resp, command):
+        if not resp:
+            if not self.try_connection():
+                return False
+            resp = self.send(command)
+            if not resp:
+                return False
+        return True
+
+
+sender = Sender("192.168.2.156", 4141)
+sender.start_client()
+sender.send_command("42 4")
+sleep(5)
+sender.send_command("42 1")
+# exec(open("send_command_console_python.py").read())
